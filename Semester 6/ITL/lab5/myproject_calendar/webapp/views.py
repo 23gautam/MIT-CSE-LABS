3a6b47eb0d91f339d40d240eb05bb646e490dc02
Gautam Gurrala <114209399+23gautam@users.noreply.github.com>
2024-02-08 10:47:16 +0530

    calendar

---
+++ Semester 6/ITL/lab5/myproject_calendar/webapp/views.py
@@ -0,0 +1,16 @@
+
+# Create your views here.
+from django.shortcuts import render 
+from django.http import HttpResponse 
+from datetime import date
+import calendar
+from calendar import HTMLCalendar
+
+def index(request,year,month):
+    year=int(year)
+    month=int(month)
+    if year<1900 or year> 2099:year=date.today().year
+    month_name = calendar.month_name[month]
+    title = "My club event calendar - %s %s"%(month_name,year)
+    cal = HTMLCalendar().formatmonth(year,month)
+    return HttpResponse("<h1>%s<h1><p>%s</p>"%(title,cal))
